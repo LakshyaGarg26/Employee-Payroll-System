67922e53cd08ea8efcfd4078a0ce879f8aff4676
LakshyaGarg26 <lakshya0418.be20@chitkara.edu.in>
2023-08-11 09:01:31 +0530

    Project completed

--- Main.java
+++ Main.java
@@ -80,11 +80,28 @@ class PayrollSystem{
             employeeList.remove(employeeToRemove);
         }
     }
+
+    public void displayEmployees(){
+        for (Employee employee : employeeList) {
+            System.out.println(employee);
+        }
+    }
 }
 
 
 public class Main{
     public static void main(String[] args) {
-        
+        PayrollSystem payrollSystem = new PayrollSystem();
+        FullTimeEmployee emp1 = new FullTimeEmployee("Vikas",1,70000.0);
+        PartTimeEmployee emp2 = new PartTimeEmployee("Alexander", 2, 40, 100);
+
+        payrollSystem.addEmployee(emp1);
+        payrollSystem.addEmployee(emp2);
+        System.out.println("Initial Employee Details: ");
+        payrollSystem.displayEmployees();
+        System.out.println("Removing Employees");
+        payrollSystem.removeEmployee(2);
+        System.out.println("Remaining Employees Details");
+        payrollSystem.displayEmployees();
     }
 }
